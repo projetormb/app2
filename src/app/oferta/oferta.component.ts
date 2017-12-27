ab769d67e53b7e2cdef127d5dd772b197e1da0fe
rafael <projetormb@gmail.com>
2017-12-27 15:52:27 -0200

    Uploading project

---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router'
+import { Oferta } from '../shared/oferta.model'
+import { OfertasService } from '../ofertas.service'
+
+
+
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
+})
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  public oferta: Oferta
+
+  constructor(
+    private route: ActivatedRoute, 
+    private ofertasService: OfertasService
+    ) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe((parametros: Params) => {
+
+      this.ofertasService.getOfertaPorId(parametros.id)
+      .then(( oferta: Oferta ) => {
+        this.oferta = oferta
+      })      
+      console.log(parametros.id)
+    })
+
+
+    
+  }
+
+  ngOnDestroy() {
+    
+  }
+}
---
+++ src/app/util/descricao-reduzida.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({ 
+    name: 'descricaoReduzida'
+})
+export class DescricaoReduzida implements PipeTransform {
+    
+    transform(texto: string, truncarEm: number, iniciarEm: number): string {
+        if (texto.length >= truncarEm) {
+            return texto.substr(iniciarEm, truncarEm) + '... '
+        }
+
+        return texto
+    }
+}
